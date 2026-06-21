34abedc91d3884772d79bd1b0fa34ab2beff5dac
ShineSky <1058131716@qq.com>
2026-06-21 18:13:40 +0800

    初始化项目

---
+++ Source/ChemicalBond/ChemicalBond.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChemicalBond.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChemicalBond, "ChemicalBond" );
